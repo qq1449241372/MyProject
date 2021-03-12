54839be9832288ec24c237879216ad85d9542a22
Dk <496908054@qq.com>
2021-03-12 19:13:08 +0800

    tabbar

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <tab-bar>
+      <tab-bar-item path="/home" >
+        <img src="./assets/img/home.svg" alt="" slot="item-icon" >
+        <img src="./assets/img/home_actived.svg" alt="" slot="item-icon-actived">
+        <div name="item-icon" slot="item-text">Home</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img src="./assets/img/bag.svg" alt="" slot="item-icon">
+        <img src="./assets/img/bag_actived.svg" alt="" slot="item-icon-actived">
+        <div name="item-icon" slot="item-text">ShoppingCart</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img src="./assets/img/my.svg" alt="" slot="item-icon">
+        <img src="./assets/img/my_actived.svg" alt="" slot="item-icon-actived">
+        <div name="item-icon" slot="item-text">My</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+<script>
+  import TabBar from "./components/tabbar/TabBar";
+  import TabBarItem from "./components/tabbar/TabBarItem";
+
+  export default {
+    name: 'App',
+    components: {
+      TabBar,
+      TabBarItem,
+    }
+  }
+</script>
+<style>
+  @import "./assets/css/base.css";
+</style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <slot name="item-icon"></slot>
+    <slot name="item-icon-actived"></slot>
+    <slot name="item-text"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    props: {
+      path: String
+    },
+    data() {
+      return {
+        isActive: true,
+      }
+    },
+    methods: {
+      itemClick() {
+        this.$router.replace(this.path)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    margin-bottom: 2px;
+    vertical-align: middle;
+
+  }
+
+</style>
